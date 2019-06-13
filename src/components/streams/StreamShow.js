1cb1283021ddf67c4485c88afc8c20363e9acf63
Divyanshu Bhatnagar <bhatnagardivyanshu@gmail.com>
2019-06-13 21:11:41 +0530

    ADD - Video Streaming/Stream Show

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,11 +1,44 @@
 import React from "react";
+import flv from 'flv.js';
 import { connect } from "react-redux";
 import { fetchStream } from "../../actions";
 
 class StreamShow extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.videoRef = React.createRef();
+  }
+  
   componentDidMount() {
     this.props.fetchStream(this.props.match.params.id);
+    this.initVideo(this.props.match.params.id);
   }
+
+  componentDidUpdate() {
+    // this runs whenever our component is re-rendered which it will be when we fetch stream
+    this.initVideo(this.props.match.params.id);
+  }
+  
+  componentWillUnmount() {
+    this.player.destroy();
+  }
+  
+  initVideo(streamId) {
+    if (this.player || !this.props.stream) {
+      return;
+    }
+
+    console.log('starting player', streamId)
+    
+    this.player = flv.createPlayer({
+      type: 'flv',
+      url: `http://localhost:8000/live/${streamId}.flv`
+    });
+
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load();
+   }
 
   renderStreamDetails = () => {
     if (!this.props.stream) {
@@ -17,6 +50,7 @@ class StreamShow extends React.Component {
     return (
       <div>
         <h2 className="title">{title}</h2>
+        <video ref={this.videoRef} style={{ width: '100%' }} controls />
         <div className="description">{description}</div>
       </div>
     );
